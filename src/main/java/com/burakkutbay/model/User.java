d5085d70ceffd3406de5893f2bf32af2bda6e3d0
Burak <info@burakkutbay.com>
2018-02-05 21:47:36 +0300

    User class create.

---
+++ src/main/java/com/burakkutbay/model/User.java
@@ -0,0 +1,41 @@
+package com.burakkutbay.model;
+
+public class User {
+
+    private String name;
+    private String surname;
+    private String email;
+    private List<String> ability;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<String> getAbility() {
+        return ability;
+    }
+
+    public void setAbility(List<String> ability) {
+        this.ability = ability;
+    }
+}
