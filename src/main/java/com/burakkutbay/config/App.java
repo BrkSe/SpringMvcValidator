86bd0e49f5d147c820d1aa7b5118da079adc2129
Burak <info@burakkutbay.com>
2018-02-05 22:32:56 +0300

    App Create

---
+++ src/main/java/com/burakkutbay/config/App.java
@@ -0,0 +1,22 @@
+package com.burakkutbay.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+
+public class App extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+   @Override
+   protected Class<?>[] getRootConfigClasses() {
+      return new Class[] {};
+   }
+
+   @Override
+   protected Class<?>[] getServletConfigClasses() {
+      return new Class[] { WebConfig.class };
+   }
+
+   @Override
+   protected String[] getServletMappings() {
+      return new String[] { "/" };
+   }
+}
